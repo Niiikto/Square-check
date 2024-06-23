ecc300e8be1bb02a1657d08f4cc96f00f762928c
dariya_alimbetova <98909655+Niiikto@users.noreply.github.com>
2024-06-23 00:43:18 -0700

    square_cube.c
    I added function that checks if the number is cube

--- square.c
+++ square.c
@@ -19,24 +19,53 @@ bool in_squares(int num, int size, int squares[]) {
     return false;
 }
 
+void fill_pow3(int pow3[], int size) {
+    for (int i = 0; i < size; i++) {
+        pow3[i] = i * i * i;
+    }
+}
+
+bool in_pow3(int num2, int size, int pow3[]) {
+    for (int i = 0; i < size; i++) {
+        if (pow3[i] == num2) {
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(){
     int sqr[MAX_SIZE];
+    int cb[MAX_SIZE];
     
 
     fill_squares(sqr, MAX_SIZE);
+    fill_pow3(cb, MAX_SIZE);
     
 
-    int input;
-    printf("Please enter number to check: ");
-    scanf("%d", &input);
+    int input_sq;
+    int input_cb;
+    printf("Please enter number to check if it is a square: ");
+    scanf("%d", &input_sq);
+    printf("Please enter number to check if it is a cube: ");
+    scanf("%d", &input_cb);
     
     
 
-    if(in_squares(input, MAX_SIZE, sqr)){
-        printf("%d = %.0f^2", input, sqrt(input));
+    if(in_squares(input_sq, MAX_SIZE, sqr)){
+        printf("%d = %.0f^2", input_sq, sqrt(input_sq));
     }
     else{
         printf("Not a square");
+    }
+
+    printf("\n");
+    
+    if(in_pow3(input_cb, MAX_SIZE, cb)){
+        printf("%d = %.0f^3", input_cb, cbrt(input_cb));
+    }
+    else{
+        printf("Not a cube");
     }
 
     printf("\n");
